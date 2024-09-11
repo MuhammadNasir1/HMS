b01e4ea8b4d53aed42f667aed929291dfb9cfe2f
MuhammadNasir1 <nmian7080@gmail.com>
2024-09-11 21:52:32 +0500

    V1

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])++
 </head>
 
 <body>
-    <h1>helllo word</h1>
+
 </body>
 
 </html>
